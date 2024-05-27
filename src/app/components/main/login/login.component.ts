a00875d16c7826b83b960aa3dccf7a10fad614bf
ShadowNightCoder <barbar0608@gmail.com>
2024-05-27 15:00:30 +0300

    last update

--- src/app/components/main/login/login.component.ts
+++ src/app/components/main/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { user } from '../../interface/user.interface';
 import { localStroageService } from '../../service/localstorage.service';
+import { FormService } from '../../service/form.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -16,18 +18,25 @@ export class LoginComponent {
     email: '',
     password: ''
   };
-
-  constructor(private localstorage: localStroageService){}
-
+  loginForm!: FormGroup;
 
 
-  onSubmit(element: NgForm) {
-    this.loggedUser.email = element.value.email;
-    this.loggedUser.password = element.value.password;
-    element.resetForm();
-    this.emailStatus = '';
-    this.passwordStatus = '';
+  constructor(private localstorage: localStroageService, private formService: FormService, private router: Router){}
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email, this.formService.forbiddenCharacter.bind(this)]), // Apply required and email validators
+      'password': new FormControl(null, [Validators.required, this.formService.forbiddenCharacter.bind(this)]) // Apply required validator
+    });
+  }
+
+  onSubmit() {
+    this.loggedUser.email = this.loginForm.value.email;
+    this.loggedUser.password = this.loginForm.value.password;
+    this.loginForm.reset();
     this.localstorage.setUserInLocalStroage(this.loggedUser)
+    this.router.navigateByUrl('/home');
 
   }
+
 }
